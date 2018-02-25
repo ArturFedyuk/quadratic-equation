9e37dfa0c38ca5247a2cec46eccd4449e1c93477
Artur <artur-fedyuk@mail.ru>
2018-02-25 23:29:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    result = [];
+
+    result[0] = Math.round((-equation[1] + D ** 0.5) / (2 * equation[0]));
+    result[1] = Math.round((-equation[1] - D ** 0.5) / (2 * equation[0]));
+
+    return result.sort((a,b) => a - b)
+};
