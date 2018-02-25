0d1fe00b69265452d2b443ac6c5e4a2aaf989981
Artur <artur-fedyuk@mail.ru>
2018-02-25 23:33:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function solveEquation(equation) {
   equation = equation.replace(/\s/g,'');
-  arr = equation.split('x');
+  let arr = equation.split('x');
 
-  A = Int(arr[0]),
-  B = Int(arr[1].slice(2)),
-  C = Int(arr[2]);
+  const A = parseInt(arr[0]),
+        B = parseInt(arr[1].slice(2)),
+        C = parseInt(arr[2]);
 
-  D = Math.pow(B,2) - 4*A*C,
-  X1 = Math.round((-B + Math.sqrt(D)) / (2 * A)),
-  X2 = Math.round((-B - Math.sqrt(D)) / (2 * A));
+  let D = Math.pow(B,2) - 4*A*C,
+      X1 = Math.round((-B + Math.sqrt(D)) / (2 * A)),
+      X2 = Math.round((-B - Math.sqrt(D)) / (2 * A));
 
   return [X1, X2].sort( (a,b) => a - b );
 }
